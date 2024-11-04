68ecc24dc10c906b852d573643dc6b4044496a3c
LimbergDV <limbergdelvalle@gmail.com>
2024-11-04 12:57:28 -0600

    implementando objetivos

--- src/app/juego/juego.component.ts
+++ src/app/juego/juego.component.ts
@@ -9,6 +9,7 @@ import { Dado } from '../interfaces/dado';
 export class JuegoComponent {
   dados: Dado[] = [];
   chances: number = 3;
+  recomendaciones: string[] = [];
 
   constructor() {
     this.inicializarDados();
@@ -33,6 +34,7 @@ export class JuegoComponent {
         }
       });
       this.chances--;
+      this.evaluarDados();
     }
   }
 
@@ -47,5 +49,37 @@ export class JuegoComponent {
 
   evaluarDados(){
     const contador = Array(7).fill(0);
+    this.dados.forEach(dado => contador[dado.value]++);
+
+    const tieneTresDeUnTipo = contador.some(count => count >= 3);
+    const tieneCuatroDeUnTipo = contador.some(count => count >= 4);
+    const tieneFullHouse = contador.filter(count => count === 3).length === 1 && contador.filter(count => count === 2).length === 1;
+    const tieneYatzy = contador.some(count => count === 5);
+    const esSecuenciaBaja = this.verificarSecuenciaBaja(contador);
+    const esSecuenciaAlta = this.verificarSecuenciaAlta(contador);
+
+    this.recomendaciones = [];
+
+
+    for (let i = 1; i <= 6; i++) {
+      if (contador[i] > 0) {
+        this.recomendaciones.push(`${contador[i]} ${i}s`);
+      }
+    }
+
+    if (tieneTresDeUnTipo) this.recomendaciones.push('Tres de un tipo');
+    if (tieneCuatroDeUnTipo) this.recomendaciones.push('Cuatro de un tipo');
+    if (tieneYatzy) this.recomendaciones.push('¡Yatzy! (Cinco de un tipo)');
+    if (tieneFullHouse) this.recomendaciones.push('Full House (Tres de un tipo y dos de otro)');
+    if (esSecuenciaBaja) this.recomendaciones.push('Secuencia baja');
+    if (esSecuenciaAlta) this.recomendaciones.push('Secuencia alta');
+  }
+
+  verificarSecuenciaBaja(contador: number[]): boolean {
+    return contador[1] > 0 && contador[2] > 0 && contador[3] > 0 && contador[4] > 0;
+  }
+
+  verificarSecuenciaAlta(contador: number[]): boolean {
+    return contador[2] > 0 && contador[3] > 0 && contador[4] > 0 && contador[5] > 0;
   }
 }
